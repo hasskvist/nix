717d0b1d8ddf60ef5b2fcfaaae09d3d2f3c6ccf5
Tobbelito <tobbe@flipperspel.nu>
2025-04-11 23:01:58 +0200

    remove admin user

--- hosts/panel/default.nix
+++ hosts/panel/default.nix
@@ -48,19 +48,6 @@ in
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.admin = {
-    isNormalUser = true;
-    description = "admin";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    packages = with pkgs; [
-      # install user packages here
-    ];
-  };
-
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "tobias";
@@ -81,5 +68,4 @@ in
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
