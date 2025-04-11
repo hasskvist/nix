aff7bc678a13967ed41ad50053c6ed40a9670b86
Tobbelito <tobbe@flipperspel.nu>
2025-04-12 00:57:25 +0200

    configure font paths kinda fhs like

--- hosts/_shared/gui/fonts.nix
+++ hosts/_shared/gui/fonts.nix
@@ -1,4 +1,9 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   modName = "fonts";
   cfg = config.tob.${modName};
@@ -11,6 +16,36 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    system.fsPackages = [ pkgs.bindfs ];
+
+    fileSystems =
+      let
+        mkRoSymBind = path: {
+          device = path;
+          fsType = "fuse.bindfs";
+          options = [
+            "ro"
+            "resolve-symlinks"
+            "x-gvfs-hide"
+          ];
+        };
+        aggregated = pkgs.buildEnv {
+          name = "system-fonts-and-icons";
+          paths =
+            config.fonts.packages
+            ++ (with pkgs; [
+              # Add your cursor themes and icon packages here
+            ]);
+          pathsToLink = [
+            "/share/fonts"
+            "/share/icons"
+          ];
+        };
+      in
+      {
+        "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
+        "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
+      };
     fonts = {
       # Install a bunch of default fonts
       enableDefaultPackages = true;
@@ -19,7 +54,12 @@ in
       fontDir.enable = true;
 
       # Install Hack nerdfont
-      packages = [ pkgs.nerd-fonts.hack ];
+      packages = [
+        pkgs.nerd-fonts.hack
+        pkgs.noto-fonts
+        pkgs.noto-fonts-emoji
+        pkgs.noto-fonts-cjk-sans
+      ];
     };
   };
 }
