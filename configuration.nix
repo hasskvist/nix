5d1e3ed9723693d8d2407aa2e725bd09e8e7eeef
Tobbelito <tobbe@flipperspel.nu>
2025-04-11 20:20:40 +0200

    use HOSTNAME variable to decide config

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,35 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports = [ ./hosts/hplt ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  # When running nixos-install or nixos-rebuild HOSTNAME
+  # must be set manually the first time(until HOSTNAME is in all your shells)
+  hostname = lib.trim (builtins.getEnv "HOSTNAME");
+in
+{
+  # Set hostname based on HOSTNAME environment variable
+  networking.hostName = hostname;
+
+  # Set HOSTNAME variable s
+  environment.variables = {
+    HOSTNAME = config.networking.hostName;
+  };
+
+  imports =
+    if hostname == "hplt" then
+      [
+        ./hosts/hplt
+      ]
+    else if hostname == "panel" then
+      [
+        ./hosts/panel
+      ]
+      ++ [
+        # shared modules
+      ]
+    else
+      builtins.throw "environment variable HOSTNAME must be set";
 }
