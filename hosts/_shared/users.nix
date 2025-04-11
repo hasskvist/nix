61f6e9570a6a3a483e841294974c9e316e7e3708
Tobbelito <tobbe@flipperspel.nu>
2025-04-11 22:02:20 +0200

    Set tobias and root password
    Pls don't hack

--- hosts/_shared/users.nix
+++ hosts/_shared/users.nix
@@ -1,17 +1,26 @@
 { config, pkgs, ... }:
+let
+  hashedPassword = "$2b$05$UV9X2REQDqC0l6Bul5HMIOZ9SaQldn3NAnS854XE58klpgaYdofga";
+in
 {
-  users.users.tobias = {
-    isNormalUser = true;
-    description = "Tobias";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    shell = config.programs.fish.package;
-    packages = with pkgs; [
-      kdePackages.kate # Text editor
-      google-chrome # Shitty web-browser
-    ];
+  users.users = {
+    root = {
+      inherit hashedPassword;
+    };
+    tobias = {
+      inherit hashedPassword;
+      isNormalUser = true;
+      description = "Tobias";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      shell = config.programs.fish.package;
+      packages = with pkgs; [
+        kdePackages.kate # Text editor
+        google-chrome # Shitty web-browser
+      ];
+    };
   };
   home-manager = {
     # If a file exists that home-manager wants to write to we rename the existing
