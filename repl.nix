94972ad0dbb682d3524be1813eeeaca3b5397721
Tobbelito <tobbe@flipperspel.nu>
2025-04-11 22:22:20 +0200

    make home manager available under home in repl

--- repl.nix
+++ repl.nix
@@ -3,12 +3,16 @@ let
   system = builtins.currentSystem;
   # Get the function that's used to build a NixOS system
   eval-config = import <nixos/nixos/lib/eval-config.nix>;
+  # Call eval-config with current system
+  # Everything in our NixOS config goes through configuration.nix as entrypoint
+  nixos = eval-config {
+    inherit system;
+    modules = [
+      ./configuration.nix
+    ];
+  };
+  home = nixos.config.home-manager.users.tobias;
 in
-# Call eval-config with current system
-# Everything in our NixOS config goes through configuration.nix as entrypoint
-eval-config {
-  inherit system;
-  modules = [
-    ./configuration.nix
-  ];
+nixos // {
+  inherit home;
 }
