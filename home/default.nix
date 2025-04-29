fa260f534f6b6c2e795546f900dffa9ced195cb0
Tobbelito <tobbe@flipperspel.nu>
2025-04-29 22:15:09 +0200

    Fix deprecated lsd alias disable

--- home/default.nix
+++ home/default.nix
@@ -24,36 +24,43 @@
 
     # Enable HM bash config
     programs.bash.enable = true;
+
     # Enable HM fish config
     programs.fish = {
       enable = true;
       # Use same fish package as configured in NixOS
       package = nixosConfig.programs.fish.package;
     };
+
     # Enable bat, a modern cat alternative
     programs.bat.enable = true;
+
     # Enable ripgrep(rg), a modern grep alternative
     programs.ripgrep.enable = true;
+
     # Enable direnv (Executes shell scripts when you enter directories)
     programs.direnv.enable = true;
+
     # Enable tealdeer (command: tldr to find common CLI usage)
     programs.tealdeer = {
       enable = true;
       enableAutoUpdates = true;
     };
+
     # Enable LSD, a modern ls alternative
-    programs.lsd = {
-      enable = true;
-      enableAliases = true;
-    };
+    programs.lsd.enable = true;
+
     # Enable git
     programs.git = {
       enable = true;
     };
+
     # Enable gitui, a nice git TUI
     programs.gitui.enable = true;
+
     # Enable fd, a modern find alternative
     programs.fd.enable = true;
+
     # Configure XDG
     xdg = {
       enable = true;
